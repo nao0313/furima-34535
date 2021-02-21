ca80459defecd57b3ddf57f8bbe9722dfed9acf8
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-02-21 22:43:48 +0900

    gimeiの導入、FactoryBotの更新

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,16 @@
 FactoryBot.define do
   factory :user do
+    transient do
+      person { Gimei.name }
+    end
     email                 { Faker::Internet.free_email }
     password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     nickname              { Faker::Name.initials(number: 2) }
-    last_name             { Faker::person.last.kanji }
-    first_name            { Faker::person.first.kanji }
-    last_name_kana        { Faker::person.last.katakana }
-    first_name_kana       { Faker::person.first.katakana }
+    last_name             { person.last.kanji }
+    first_name            { person.first.kanji }
+    last_name_kana        { person.last.katakana }
+    first_name_kana       { person.first.katakana }
     birthday              { Faker::Date.in_date_period }
   end
 end

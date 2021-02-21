556392e5c38e2a389ac785bb4126385fea8e5e19
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-02-21 21:23:51 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname,        presence: true
-         validates :last_name,       presence: true
-         validates :first_name,      presence: true
-         validates :last_name_kana,  presence: true
-         validates :first_name_kana, presence: true
+         validates :nickname,        presence: true, length: { maximum: 40 }
+         validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+         validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+         validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶ]+\z/}
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/}
          validates :birthday,        presence: true
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end

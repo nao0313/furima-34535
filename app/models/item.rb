d4a413b4d3aec6a96397718e4a06c1714757adb1
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-02-22 20:35:19 +0900

    バリデーションを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,20 @@
 class Item < ApplicationRecord
+
   with_options presence: true do
     validates :name
     validates :description
-    validates :detail_id
-    validates :state_id
-    validates :sipping_cost_id
-    validates :prefecture_id
-    validates :selling_price
     validates :image
+
+    with_options numericality: { other_than: 1 } do
+      validates :detail_id
+      validates :state_id 
+      validates :sipping_cost_id
+      validates :prefecture_id
+      validates :selling_price_id
+    end
   end
 
+  
   belongs_to :user
   has_one_attached :image
 

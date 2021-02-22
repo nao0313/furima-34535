6eebb3fe33931f32f36a1f7e0193801fbece339b
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-02-22 20:45:49 +0900

    newアクションへのコントローラー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
+  
+  def new
+    @items = Item.order("created_at DESC")
+  end
+
+  private
+
+  def article_params
+    params.require(:item).permit(:name, :description, :detail_id, :state_id, :sipping_cost_id, :prefecture_id, :selling_price_id, :user_id )
+  end
+
 end

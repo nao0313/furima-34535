c905d25bdaf8ee7c140e8c35d0142db9adb8e41c
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-02-22 20:28:39 +0900

    ActiveHashとのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,23 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :detail_id
+    validates :state_id
+    validates :sipping_cost_id
+    validates :prefecture_id
+    validates :selling_price
+    validates :image
+  end
+
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :detail
+  belongs_to :state
+  belongs_to :sipping_cost
+  belongs_to :prefecture
+  belongs_to :selling_price
+
 end

84119e14af79092fc43534bc927579a42565e9d1
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-02-23 12:21:37 +0900

    手数料、利益の計算結果の表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+   const inputValue = priceInput.value;
+   console.log(inputValue);
+      const addTaxDom = document.getElementById("add-tax-price");
+      addTaxDom.innerHTML = inputValue * 0.1
+      console.log(addTaxDom);
+      const addProfit = document.getElementById("profit");
+      addProfit.innerHTML = inputValue - (inputValue * 0.1)
+      console.log(addProfit);
+})
+});

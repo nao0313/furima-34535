1ff038676175ffc738047815525dc0a921a99360
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-02-24 14:55:35 +0900

    レビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,7 +42,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @item.user_id
+    if current_user.id ! = @item.user_id
       redirect_to action: :index
     end
   end

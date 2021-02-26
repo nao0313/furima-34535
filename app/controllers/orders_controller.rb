6e48e93966c39865c52fdf144c775e4469e8e605
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-02-26 15:46:34 +0900

    購入画面の表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,24 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new
   end
 
   def create
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+      @order_shipping.save
+      rendirecto_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shipping).permit(:postal_code, :prefecture_id, :city, :address, :phone_number).merge(user_id: current_user.id)
   end
 
 end

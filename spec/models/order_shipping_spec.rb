2b7c56fceb1bc9953c58053192ae6a674f774afe
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-02-27 20:47:19 +0900

    テストコードの追加

--- spec/models/order_shipping_spec.rb
+++ spec/models/order_shipping_spec.rb
@@ -64,6 +64,16 @@ RSpec.describe OrderShipping, type: :model do
         @order_shipping.valid?
         expect(@order_shipping.errors.full_messages).to include("Token can't be blank")
       end
+      it 'item_idがない場合は購入できない' do
+        @order_shipping.item_id = ''
+        @order_shipping.valid?
+        expect(@order_shipping.errors.full_messages).to include("Item can't be blank")
+      end
+      it 'user_idがない場合は購入できない' do
+        @order_shipping.user_id = ''
+        @order_shipping.valid?
+        expect(@order_shipping.errors.full_messages).to include("User can't be blank")
+      end
     end
   end
 end

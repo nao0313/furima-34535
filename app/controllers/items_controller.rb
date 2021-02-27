c5460a60f15be1a9f13a9a9a7a16785db516f3fc
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-02-27 21:23:59 +0900

    beforeアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
   before_action :move_to_index, only: [:edit, :update, :destroy]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy, :move_to_top_page]
   before_action :move_to_top_page
+  
   def index
     @items = Item.all.order('created_at DESC')
   end
